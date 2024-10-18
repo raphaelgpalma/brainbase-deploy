89bcdc84f2a3903cab9288ffab2466089a546c42
raphaelgpalma <raphaelgpalma@icloud.com>
2024-10-18 02:26:52 -0300

    raphael

--- api/index.js
+++ api/index.js
@@ -3,21 +3,38 @@ const path = require('path');
 const bodyParser = require('body-parser');
 
 const app = express();
-
 const users = [];
 
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, '..', 'public'))); // Ajustar o caminho
+app.use(express.static(path.join(__dirname, '../public'))); // Ajustar o caminho para a pasta public
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '..', 'public', 'loading.html'));
+    res.sendFile(path.join(__dirname, '../public', 'loading.html'));
 });
 
 app.get('/cadastro', (req, res) => {
-    res.sendFile(path.join(__dirname, '..', 'public', 'cadastro.html'));
+    res.sendFile(path.join(__dirname, '../public', 'cadastro.html'));
 });
 
-// ... outras rotas ...
+app.get('/projetos', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public', 'ProjetosHTML.html'));
+});
+
+app.get('/tarefas', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public', 'tarefas.html'));
+});
+
+app.get('/projetosEmGrupo', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public', 'projetosEmGrupo.html'));
+});
+
+app.get('/login', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public', 'login.html'));
+});
+
+app.get('/menu-inicial', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public', 'menuInicial.html'));
+});
 
 app.post('/cadastro', (req, res) => {
     const { name, email, password, confirm_password } = req.body;
@@ -36,8 +53,19 @@ app.post('/cadastro', (req, res) => {
     res.redirect('/login');
 });
 
-// ... outras rotas ...
+app.post('/login', (req, res) => {
+    const { email, password } = req.body;
 
+    const user = users.find(user => user.email === email && user.password === password);
+
+    if (!user) {
+        return res.status(401).send('Credenciais inválidas. <a href="/login">Tente novamente</a>');
+    }
+
+    res.redirect('/menu-inicial');
+});
+
+// Roteia todas as outras solicitações para um 404
 app.use((req, res) => {
     res.status(404).send('Página não encontrada');
 });
