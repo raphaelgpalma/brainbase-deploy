f7931f4a6075cb555efa53ea9732b14e02b9a7ea
Raphael Palma <117413668+raphaelgpalma@users.noreply.github.com>
2024-10-24 20:31:50 -0300

    Update cadastro.js

--- public/cadastro.js
+++ public/cadastro.js
@@ -1,23 +1,7 @@
-document.querySelector('form').addEventListener('submit', async function(event) {
+document.querySelector('form').addEventListener('submit', function(event) {
     event.preventDefault();
 
-    const name = document.querySelector('#name').value;
-    const email = document.querySelector('#email').value;
-    const password = document.querySelector('#password').value;
-    const confirmPassword = document.querySelector('#confirm-password').value;
-
-    const response = await fetch('/cadastro', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/x-www-form-urlencoded'
-        },
-        body: `name=${name}&email=${email}&password=${password}&confirm_password=${confirmPassword}`
-    });
-
-    if (response.redirected) {
-        window.location.href = '/login'; // Redireciona para a página de login
-    } else {
-        const result = await response.text();
-        document.body.innerHTML = result;
-    }
+    // Simula um cadastro bem-sucedido (independente dos dados inseridos)
+    window.location.href = '/login'; // Redireciona sempre para a página de login
 });
+
