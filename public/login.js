274cbea94eaeac1d03cd5b39e14c8275377a5536
Raphael Palma <117413668+raphaelgpalma@users.noreply.github.com>
2024-10-24 20:37:50 -0300

    Update login.js

--- public/login.js
+++ public/login.js
@@ -2,5 +2,5 @@ document.querySelector('form').addEventListener('submit', function(event) {
     event.preventDefault();
 
     // Simula um login bem-sucedido (independente de credenciais)
-    window.location.href = '/menu-inicial'; // Redireciona sempre para o menu inicial
+    window.location.href = 'https://brainbase-deploy.vercel.app/menu-inicial'; // Redireciona sempre para o menu inicial
 });
