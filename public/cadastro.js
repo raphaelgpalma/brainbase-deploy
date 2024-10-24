3b5816f72c6e77f5c52a56fa54e02ff0d6dae797
Raphael Palma <117413668+raphaelgpalma@users.noreply.github.com>
2024-10-24 20:38:11 -0300

    Update cadastro.js

--- public/cadastro.js
+++ public/cadastro.js
@@ -2,6 +2,6 @@ document.querySelector('form').addEventListener('submit', function(event) {
     event.preventDefault();
 
     // Simula um cadastro bem-sucedido (independente dos dados inseridos)
-    window.location.href = '/login'; // Redireciona sempre para a página de login
+    window.location.href = 'https://brainbase-deploy.vercel.app/login'; // Redireciona sempre para a página de login
 });
 
